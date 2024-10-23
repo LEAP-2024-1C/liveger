82d7885da8b1a430e46529b773d6f47e0412708c
angaa123 <75901266+angaa123@users.noreply.github.com>
2024-10-23 13:15:44 +0800

    suir hiij baina

---
+++ frontend/src/app/host/(home)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Host_header from "@/components/host_header";
+
+function Page() {
+	return (
+		<div>
+			<Host_header />
+			<div>Page</div>
+		</div>
+	);
+}
+
+export default Page;
