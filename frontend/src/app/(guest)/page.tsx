dc5b590b8f0307c36d61ea8aa3f91a186c704527
erkaerka21 <eegiibaagii597@gmail.com>
2024-10-25 12:24:49 +0800

    1025 angid

--- frontend/src/app/(guest)/page.tsx
+++ frontend/src/app/(guest)/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Place from "./(main)/place/page";
 
 export default function Home() {
-	return <div className=""></div>;
+  return (
+    <div className="">
+      <Place />
+    </div>
+  );
 }
