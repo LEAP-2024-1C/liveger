dda197340c194a07afab509cd0d5a3f69fe3770c
erkaerka21 <eegiibaagii597@gmail.com>
2024-10-26 23:11:34 +0800

    1026 cart bolon cartnii detail

--- frontend/src/app/(guest)/page.tsx
+++ frontend/src/app/(guest)/page.tsx
@@ -1,11 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Place from "./(main)/place/page";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Place />
-    </div>
-  );
+  return <div className=""></div>;
 }
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import UserHome from "./components/userhome";
+
+export default function Home() {
+  return (
+    <div className="">
+      <UserHome />
+    </div>
+  );
+}
