97432ae7e7eaa14124b551b1fe09c6fa1cadd308
erkaerka21 <eegiibaagii597@gmail.com>
2024-10-28 10:48:52 +0800

    1028 backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDatabase from "./config/database";
+
 dotenv.config();
 const PORT = process.env.PORT;
-
+const mongoUrl = process.env.URL || "";
 const app = express(); //express function ajillaad app gdg object uusgej bn
 app.use(express.json());
 app.use(cors());
+
+connectDatabase(mongoUrl);
 app.listen(PORT, () => {
   console.log(`сервер ${PORT} дээр ажиллаж байна.`);
 });
