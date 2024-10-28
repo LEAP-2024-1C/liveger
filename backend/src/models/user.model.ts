8b483e31175dc734eadb5fb18ca43bf7679f7385
Muugii9902 <muugii9902@gmail.com>
2024-10-28 17:28:14 +0800

    backlogin

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,1 +1,60 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
+interface IUser {
+  _id: Schema.Types.ObjectId;
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  password: string;
+  role: "user" | "host";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    phoneNumber: String,
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["user", "host"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  } else {
+    const hashedPassword = bcrypt.hashSync(this.password.toString(), 10);
+    this.password = hashedPassword;
+    next();
+  }
+});
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
