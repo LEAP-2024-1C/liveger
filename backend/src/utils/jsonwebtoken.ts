aa01362aad8cfb5feaf8d83476a04c5145ceab46
erkaerka21 <eegiibaagii597@gmail.com>
2024-10-28 11:54:19 +0800

    1028 auth bolon loggeriin middleware uusgesen

---
+++ backend/src/utils/jsonwebtoken.ts
@@ -0,0 +1,11 @@
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+export const generateToken = (payload: object) => {
+  return jsonwebtoken.sign(payload, process.env.JSON_TOKEN_PASSWORD || "", {
+    expiresIn: 30 * 24 * 60 * 60,
+  });
+};
+export const decodeToken = (token: string) => {
+  return jsonwebtoken.verify(token, process.env.JSON_TOKEN_PASSWORD || "");
+};
