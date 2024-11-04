bdb790a851dea2b515f522552dab6f0697fe432d
Muugii9902 <muugii9902@gmail.com>
2024-11-04 19:13:45 +0800

    nuuts sergeehed aldaa

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
+
 import { generateToken } from "../utils/jsonwebtoken";
+import { sendPasswordResetEmail } from "../utils/sendmail";
 
 export const signup = async (req: Request, res: Response) => {
   try {
@@ -47,5 +50,78 @@ export const login = async (req: Request, res: Response) => {
     res.status(200).json({ message: "success", token });
   } catch (error) {
     res.status(500).json({ message: "Серверийн алдаа", error });
+  }
+};
+export const forgetPassword = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+    const findUser = await User.findOne({ email });
+    console.log("email", email);
+
+    if (!findUser) {
+      return res.status(404).json({
+        success: false,
+        message: "Бүртгэлтэй хэрэглэгч олдсонгүй",
+      });
+    }
+
+    const resetToken = crypto.randomBytes(32).toString("hex");
+
+    const hashedToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+
+    findUser.resetPasswordToken = hashedToken;
+    findUser.resetPasswordExpires = new Date(Date.now() + 10 * 60 * 1000);
+    await findUser.save();
+
+    await sendPasswordResetEmail(email, resetToken);
+
+    res.status(200).json({
+      success: true,
+      message: "Нууц үг сэргээх линк илгээгдлээ",
+    });
+  } catch (error) {
+    console.error("Forgot password error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Серверийн алдаа гарлаа",
+    });
+  }
+};
+export const verifyPassword = async (req: Request, res: Response) => {
+  try {
+    const { password, resetToken } = req.body;
+    const hashedResetToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+
+    const findUser = await User.findOne({
+      resetPasswordToken: hashedResetToken,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
+
+    if (!findUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Таны нууц үг сэргээх хугацаа дууссан байна",
+      });
+    }
+    findUser.password = password;
+
+    await findUser.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Нууц үг амжилттай сэргээгдлээ",
+    });
+  } catch (error) {
+    console.error("Password verification error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Серверийн алдаа гарлаа",
+    });
   }
 };
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { login, signup } from "../controllers/auth.controller";
+import {
+  forgetPassword,
+  login,
+  signup,
+  verifyPassword,
+} from "../controllers/auth.controller";
 import { currentUser } from "../controllers/user.controller";
 import { authentication } from "../middlewares/authentication";
 import { authorize } from "../middlewares/authorize";
@@ -7,5 +12,7 @@ const router = Router();
 router.route("/login").post(login);
 router.route("/signup").post(signup);
 router.route("/currentuser").get(authentication, currentUser);
+router.route("/forget-password").post(forgetPassword);
+router.route("/reset-password").post(verifyPassword);
 
 export default router;
