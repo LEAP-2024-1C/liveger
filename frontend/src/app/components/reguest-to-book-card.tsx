bb884487ad00ca35affcf053f104b543e390f0ee
Tseekii <tseekiibatanu@gmail.com>
2024-11-08 12:56:06 +0800

    111007

---
+++ frontend/src/app/components/reguest-to-book-card.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/ui/card";
+
+const RequestCard = () => {
+  return (
+    <div>
+      <h1>Request to book</h1>
+      <Card className="relative w-full min-h-fit border-green-500"></Card>
+    </div>
+  );
+};
