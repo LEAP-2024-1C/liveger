7c356f49fc9775978938c0f782248c7b6636dba4
erkaerka21 <eegiibaagii597@gmail.com>
2024-11-14 17:42:05 +0800

    fhfhfhfh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ import serviceRouter from "./routes/service.route";
 import authRouter from "./routes/auth.route";
 import placeRouter from "./routes/places.route";
 import orderRouter from "./routes/order.route";
+import { getPlaces } from "./controllers/place.controller";
 dotenv.config();
 const PORT = process.env.PORT;
 const mongoUrl = process.env.URL || "";
@@ -19,7 +20,7 @@ app.use("/api/v1/service", serviceRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/places", placeRouter);
 app.use("/api/v1/order", orderRouter);
-
+app.use("/", getPlaces);
 connectDatabase(mongoUrl);
 app.listen(PORT, () => {
   console.log(`сервер ${PORT} дээр ажиллаж байна.`);
