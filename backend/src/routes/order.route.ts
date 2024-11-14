66ef98a8f3b104c0a17eb805d6cadc9d0f0cc317
erkaerka21 <eegiibaagii597@gmail.com>
2024-11-14 11:13:42 +0800

    order confirm bolson

--- backend/src/routes/order.route.ts
+++ backend/src/routes/order.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createOrder, getOrder } from "../controllers/order.controller";
+import {
+  confirmOrder,
+  createOrder,
+  getOrder,
+} from "../controllers/order.controller";
 import { authentication } from "../middlewares/authentication";
 
 const router = Router();
 router.route("/").post(authentication, createOrder);
 router.route("/:orderId").get(authentication, getOrder);
+router.route("/confirm/:_id").put(authentication, confirmOrder);
 
 export default router;
